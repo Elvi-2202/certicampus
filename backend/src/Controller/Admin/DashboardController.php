db4b061c0fae2cc0eaa6cf8775325d093fd1dce9
Nassim <Nassim.Touissi.Etu@univ-lemans.fr>
2026-05-28 11:27:55 +0200

    Realisation du frontend du dashbord

--- backend/src/Controller/Admin/DashboardController.php
+++ backend/src/Controller/Admin/DashboardController.php
@@ -1,19 +1,33 @@
 <?php
+namespace App\Controller\Admin;
 
-namespace App\Controller\Admin; // On ajoute \Admin ici
-
+use App\Repository\CertifiedRepository;
+use App\Repository\DiplomaRepository;
+use App\Repository\SchoolRepository;
+use App\Repository\TrainingRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-// Le nom de la classe ne doit PAS avoir de slash
 final class DashboardController extends AbstractController
 {
     #[Route('/admin/dashboard', name: 'app_admin_dashboard')]
-    public function index(): Response
-    {
+    public function index(
+        CertifiedRepository $certifiedRepo,
+        DiplomaRepository $diplomaRepo,
+        SchoolRepository $schoolRepo,
+        TrainingRepository $trainingRepo,
+    ): Response {
+        $lastDiplomas = $diplomaRepo->findBy([], ['generated_at' => 'DESC'], 3);
+        $lastCertified = $certifiedRepo->findBy([], ['graduation_date' => 'DESC'], 3);
+
         return $this->render('admin/dashboard/index.html.twig', [
-            'controller_name' => 'DashboardController',
+            'count_certified'  => $certifiedRepo->count(),
+            'count_diplomas'   => $diplomaRepo->count(),
+            'count_schools'    => $schoolRepo->count(),
+            'count_trainings'  => $trainingRepo->count(),
+            'last_diplomas'    => $lastDiplomas,
+            'last_certified'   => $lastCertified,
         ]);
     }
 }
